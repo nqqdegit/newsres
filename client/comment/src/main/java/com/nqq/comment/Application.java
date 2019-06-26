47b8f3c4d52028cf3d844ca978a5dfa697aea4cb
nqqdegit <13937165721@163.com>
2019-06-26 13:34:12 +0800

    新闻网 练习springcloud

--- client/comment/src/main/java/com/nqq/comment/Application.java
+++ client/comment/src/main/java/com/nqq/comment/Application.java
@@ -2,31 +2,36 @@
  * Copyright (C), 2015-2019, 付临门有限公司
  * FileName: Application
  * Author:   user
- * Date:     2019/6/24 14:40
- * Description: 注册中心启动类
+ * Date:     2019/6/24 17:15
+ * Description: 文章模块启动类
  * History:
  * <author>          <time>          <version>          <desc>
  * 作者姓名           修改时间           版本号              描述
  */
-package com.nqq.register;
+package com.nqq.comment;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 〈一句话功能简述〉<br> 
- * 〈注册中心启动类〉
+ * 〈文章模块启动类〉
  *
  * @author user
  * @create 2019/6/24
  * @since 1.0.0
  */
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
+@EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+
     }
 }
--- client/user/src/main/java/com/nqq/user/Application.java
+++ client/user/src/main/java/com/nqq/user/Application.java
@@ -2,28 +2,34 @@
  * Copyright (C), 2015-2019, 付临门有限公司
  * FileName: Application
  * Author:   user
- * Date:     2019/6/24 14:40
- * Description: 注册中心启动类
+ * Date:     2019/6/25 9:13
+ * Description: 首页启动类
  * History:
  * <author>          <time>          <version>          <desc>
  * 作者姓名           修改时间           版本号              描述
  */
-package com.nqq.register;
+package com.nqq.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 〈一句话功能简述〉<br> 
- * 〈注册中心启动类〉
+ * 〈首页启动类〉
  *
  * @author user
- * @create 2019/6/24
+ * @create 2019/6/25
  * @since 1.0.0
  */
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
+@EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class Application {
 
     public static void main(String[] args) {
